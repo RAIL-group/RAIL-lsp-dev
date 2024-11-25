5815096f58d64cef48ce39b5b12979a1c2b0673f
gjstein <gregory.j.stein@gmail.com>
2024-11-24 21:31:49 -0500

    specify particular version of numpy

--- modules/blendervsim/blendervsim/blenderscripts/install.py
+++ modules/blendervsim/blendervsim/blenderscripts/install.py
@@ -1,6 +1,6 @@
 from subprocess import sys, call
 
-packages = ['numpy', 'scipy', 'matplotlib']
+packages = ['numpy==1.24.4', 'scipy', 'matplotlib']
 call([sys.executable,'-m','ensurepip'])
 call([sys.executable, '-m', 'pip', 'install', '-U', 'pip', 'setuptools', 'wheel'])
 call([sys.executable, '-m', 'pip', 'install'] + packages)
