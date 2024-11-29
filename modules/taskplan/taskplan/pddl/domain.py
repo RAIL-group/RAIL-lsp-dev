e653fe3614d3b9b4fb3e1cf45b6111e029ae89d8
Raihan Islam Arnob <arnob2601@yahoo.com>
2024-11-28 22:37:38 -0500

    added new actions and their required predicates

--- modules/taskplan/taskplan/pddl/domain.py
+++ modules/taskplan/taskplan/pddl/domain.py
@@ -31,18 +31,17 @@ def get_domain(whole_graph):
         (is-at ?obj - item ?loc - location)
         (rob-at ?loc - location)
         (hand-is-free)
-        (filled-with ?obj - item ?cnt - item)
-        (is-liquid ?obj - item)
         (is-pickable ?obj - item)
-        (is-fillable ?obj - item)
         (restrict-move-to ?loc - location)
-        (spread-applied ?obj1 - item ?obj2 - spread)
-        (is-spread ?obj - item)
-        (is-spreadable ?obj - item)
-        (is-washable ?obj - item)
-        (is-dirty ?obj - item)
-        (is-folded ?obj - item)
-        (is-foldable ?obj - item)
+        (is-toasted ?obj - item)
+        (is-toastable ?obj - item)
+        (is-toaster ?obj - item)
+        (is-peeled ?obj - item)
+        (is-peelable ?obj - item)
+        (is-peeler ?obj - item)
+        (is-boiled ?obj - item)
+        (is-boilable ?obj - item)
+        (is-boiler ?obj - item)
         (ban-move)
     )
 
@@ -52,36 +51,56 @@ def get_domain(whole_graph):
         (total-cost)
     )
 
-    (:action fold
-        :parameters (?npkn - napkin)
+    (:action boil
+        :parameters (?boilitem - item ?boiler - item ?loc - location)
         :precondition (and
             (hand-is-free)
-            (not (is-folded ?npkn))
-            (is-foldable ?npkn)
-            (is-at ?npkn countertop)
-            (rob-at countertop)
+            (not (is-boiled ?boilitem))
+            (is-boilable ?boilitem)
+            (is-at ?boilitem ?loc)
+            (is-boiler ?boiler)
+            (is-at ?boiler ?loc)
+            (rob-at ?loc)
         )
         :effect (and
-            (is-folded ?npkn)
-            (increase (total-cost) 50)
+            (is-boiled ?boilitem)
+            (not (ban-move))
+            (increase (total-cost) 100)
         )
     )
 
-    (:action apply-spread
-        :parameters (?s - spread ?k - knife)
+    (:action peel
+        :parameters (?peelitem - item ?peeler - item ?loc - location)
         :precondition (and
-            (rob-at countertop)
-            (is-at bread countertop)
-            (is-at ?s countertop)
-            (is-holding ?k)
-            (not (is-dirty ?k))
-            (is-spread ?s)
-            (is-spreadable bread)
-            (not (spread-applied bread ?s))
+            (not (is-peeled ?peelitem))
+            (is-peelable ?peelitem)
+            (is-at ?peelitem ?loc)
+            (is-holding ?peeler)
+            (is-peeler ?peeler)
+            (rob-at ?loc)
         )
         :effect (and
-            (spread-applied bread ?s)
-            (is-dirty ?k)
+            (is-peeled ?peelitem)
+            (not (ban-move))
+            (increase (total-cost) 100)
+        )
+    )
+
+    (:action toast
+        :parameters (?toastitem - item ?toaster - item ?loc - location)
+        :precondition (and
+            (hand-is-free)
+            (not (is-toasted ?toastitem))
+            (is-toastable ?toastitem)
+            (is-at ?toastitem ?loc)
+            (is-toaster ?toaster)
+            (is-at ?toaster ?loc)
+            (rob-at ?loc)
+        )
+        :effect (and
+            (is-toasted ?toastitem)
+            (not (ban-move))
+            (increase (total-cost) 100)
         )
     )
 
@@ -135,21 +154,6 @@ def get_domain(whole_graph):
         )
     )
 
-    ;(:action find
-    ;    :parameters (?obj - item)
-    ;    :precondition (and
-    ;        (not (is-located ?obj))
-    ;        (is-pickable ?obj)
-    ;        (hand-is-free)
-    ;    )
-    ;    :effect (and
-    ;        (is-located ?obj)
-    ;        (not (hand-is-free))
-    ;        (is-holding ?obj)
-    ;        (increase (total-cost) (find-cost ?obj))
-    ;    )
-    ;)
-
     (:action find
         :parameters (?obj - item ?from - location ?to - location)
         :precondition (and
@@ -167,77 +171,6 @@ def get_domain(whole_graph):
             (not (rob-at ?from))
             (not (ban-move))
             (increase (total-cost) (find-cost ?obj ?from ?to))
-        )
-    )
-
-    (:action fill
-        :parameters (?liquid - item ?loc - location ?cnt - item)
-        :precondition (and
-            (rob-at ?loc)
-            (is-at ?liquid ?loc)
-            (is-holding ?cnt)
-            (not (is-dirty ?cnt))
-            (is-liquid ?liquid)
-            (is-fillable ?cnt)
-            (forall (?i - item)
-                (not (filled-with ?i ?cnt))
-            )
-        )
-        :effect (and
-            (filled-with ?liquid ?cnt)
-        )
-    )
-
-    (:action pour
-        :parameters (?liquid - item ?loc - location ?cnt - item)
-        :precondition (and
-            (rob-at ?loc)
-            (is-liquid ?liquid)
-            (is-fillable ?loc)
-            (filled-with ?liquid ?cnt)
-            (is-holding ?cnt)
-        )
-        :effect (and
-            (is-at ?liquid ?loc)
-            (not (filled-with ?liquid ?cnt))
-        )
-    )
-
-    (:action make-coffee
-        :parameters (?c - item)
-        :precondition (and
-            (rob-at coffeemachine)
-            (is-at water coffeemachine)
-            (is-at coffeegrinds coffeemachine)
-            (is-fillable ?c)
-            (not (is-dirty ?c))
-            (is-at ?c coffeemachine)
-            (not (filled-with water ?c))
-            (not (filled-with coffee ?c))
-        )
-        :effect (and
-            (filled-with coffee ?c)
-            (not (is-at water coffeemachine))
-            (not (is-at coffeegrinds coffeemachine))
-            (is-dirty ?c)
-        )
-    )
-
-    (:action turn-dishwasher-on
-        :parameters ()
-        :precondition (and
-            (rob-at dishwasher)
-            ; (exists (?i - item) (is-at ?i dishwasher))
-        )
-
-        :effect (and
-            (forall
-                (?i - item)
-                (when (and (is-at ?i dishwasher) (is-dirty ?i))
-                    (not (is-dirty ?i))
-                )
-            )
-            (increase (total-cost) 50)
         )
     )
 
