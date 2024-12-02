83c1647e79862ecb529a159d22642f5e61bf94ca
gjstein <gregory.j.stein@gmail.com>
2024-12-01 21:44:46 -0500

    add a (simple) test for the overhead render

--- modules/blendervsim/tests/test_blendervsim.py
+++ modules/blendervsim/tests/test_blendervsim.py
@@ -37,3 +37,40 @@ def test_blendervsim_closes_when_errors():
     with BlenderVSim() as blender:
         with pytest.raises(RuntimeError):
             blender.error("This call should force an error in Blender")
+
+
+@pytest.mark.timeout(15)
+def test_blendervsim_render_overhead_fully_known():
+    grid = np.zeros((100, 100))
+    grid[10:90, 10:90] = 1
+    grid[15:85, 15:85] = 2
+    map_data = {
+        'occ_grid': (grid > 1.5).astype(int),
+        'semantic_labels': {'background': 1, 'free': 2},
+        'resolution': 0.1,
+        'x_offset': 0,
+        'y_offset': 0,
+    }
+
+    scene = "/resources/blender_scenes/render_overhead.blend"
+    with BlenderVSim(blender_scene_path=scene) as blender:
+        map_data['semantic_grid'] = np.ones_like(grid)
+        bkd_image, _ = blender.render_overhead(
+            map_data=map_data,
+            pixels_per_meter=25,
+            render_settings={'samples': 32, 'use_denoising': True})
+
+        map_data['semantic_grid'] = grid
+        sq_image, _ = blender.render_overhead(
+            map_data=map_data,
+            pixels_per_meter=25,
+            render_settings={'samples': 32, 'use_denoising': True})
+
+    s = bkd_image.shape
+    bkd_image_subset = bkd_image[int(0.25*s[0]):int(0.75*s[0]),
+                                int(0.25*s[1]):int(0.75*s[1])][:,:, :3]
+    sq_image_subset = sq_image[int(0.25*s[0]):int(0.75*s[0]),
+                            int(0.25*s[1]):int(0.75*s[1])][:,:, :3]
+
+    assert np.mean(bkd_image_subset) < 200
+    assert np.mean(sq_image_subset) > 200
