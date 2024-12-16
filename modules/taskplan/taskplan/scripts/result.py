87d1f732e5eb5ac5e0a8242a160d668ef1cff7ff
Raihan Islam Arnob <arnob2601@yahoo.com>
2024-12-16 18:25:27 -0500

    cleaned result, removed plotting code for now

--- modules/taskplan/taskplan/scripts/result.py
+++ modules/taskplan/taskplan/scripts/result.py
@@ -1,226 +1,6 @@
-import re
 import argparse
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from scipy.stats import gaussian_kde
 
-
-def process_learned_data(args):
-    """Preprocessing function for learned lsp"""
-    data = []
-
-    for line in open(args.data_file).readlines():
-        d = re.match(r'.*?s: (.*?) . learned: (.*?)\n', line)
-        if d is None:
-            continue
-        d = d.groups()
-        data.append([int(d[0]), float(d[1])])
-
-    return pd.DataFrame(
-        data,
-        columns=['seed', 'LEARNED_LSP']
-    )
-
-
-def process_optimistic_greedy_data(args):
-    """Preprocessing function for naive (closest action planner) lsp"""
-    data = []
-
-    for line in open(args.data_file).readlines():
-        d = re.match(r'.*?s: (.*?) . optimistic_greedy: (.*?)\n', line)
-        if d is None:
-            continue
-        d = d.groups()
-        data.append([int(d[0]), float(d[1])])
-
-    return pd.DataFrame(
-        data,
-        columns=['seed', 'OPTIMISTIC_GREEDY']
-    )
-
-
-def process_pessimistic_greedy_data(args):
-    """Preprocessing function for naive (closest action planner) lsp"""
-    data = []
-
-    for line in open(args.data_file).readlines():
-        d = re.match(r'.*?s: (.*?) . pessimistic_greedy: (.*?)\n', line)
-        if d is None:
-            continue
-        d = d.groups()
-        data.append([int(d[0]), float(d[1])])
-
-    return pd.DataFrame(
-        data,
-        columns=['seed', 'PESSIMISTIC_GREEDY']
-    )
-
-
-def process_optimistic_lsp_data(args):
-    """Preprocessing function for planner with Learned Search Policy"""
-    data = []
-
-    for line in open(args.data_file).readlines():
-        d = re.match(r'.*?s: (.*?) . optimistic_lsp: (.*?)\n', line)
-        if d is None:
-            continue
-        d = d.groups()
-        data.append([int(d[0]), float(d[1])])
-
-    return pd.DataFrame(
-        data,
-        columns=['seed', 'OPTIMISTIC_LSP']
-    )
-
-
-def process_pessimistic_lsp_data(args):
-    """Preprocessing function for planner with Learned Search Policy"""
-    data = []
-
-    for line in open(args.data_file).readlines():
-        d = re.match(r'.*?s: (.*?) . pessimistic_lsp: (.*?)\n', line)
-        if d is None:
-            continue
-        d = d.groups()
-        data.append([int(d[0]), float(d[1])])
-
-    return pd.DataFrame(
-        data,
-        columns=['seed', 'PESSIMISTIC_LSP']
-    )
-
-
-def process_optimistic_oracle_data(args):
-    """Preprocessing function for optimistic oracle"""
-    data = []
-
-    for line in open(args.data_file).readlines():
-        d = re.match(r'.*?s: (.*?) . optimistic_oracle: (.*?)\n', line)
-        if d is None:
-            continue
-        d = d.groups()
-        data.append([int(d[0]), float(d[1])])
-
-    return pd.DataFrame(
-        data,
-        columns=['seed', 'OPTIMISTIC_ORACLE']
-    )
-
-
-def process_pessimistic_oracle_data(args):
-    """Preprocessing function for pessimistic oracle"""
-    data = []
-
-    for line in open(args.data_file).readlines():
-        d = re.match(r'.*?s: (.*?) . pessimistic_oracle: (.*?)\n', line)
-        if d is None:
-            continue
-        d = d.groups()
-        data.append([int(d[0]), float(d[1])])
-
-    return pd.DataFrame(
-        data,
-        columns=['seed', 'PESSIMISTIC_ORACLE']
-    )
-
-
-def process_oracle_data(args):
-    """Preprocessing function for pessimistic oracle"""
-    data = []
-
-    for line in open(args.data_file).readlines():
-        d = re.match(r'.*?s: (.*?) . oracle: (.*?)\n', line)
-        if d is None:
-            continue
-        d = d.groups()
-        data.append([int(d[0]), float(d[1])])
-
-    return pd.DataFrame(
-        data,
-        columns=['seed', 'ORACLE']
-    )
-
-
-def process_data(args):
-    """Preprocessing function for all planner"""
-    learned_data = process_learned_data(args)
-    temp = args.data_file
-    args.data_file = args.data_file2
-    if args.scatter2:
-        other_data = process_learned_sp_data(args)
-    else:
-        other_data = process_naive_data(args)
-    args.data_file = temp
-    return other_data, learned_data
-
-
-def gjs_scatter_plot(ax, cost_x, cost_y, max_val, fail_val):
-    common_seeds = set.intersection(set(cost_x.keys()), set(cost_y.keys()))
-    y_axis = [cost_y[seed] for seed in common_seeds]
-    x_axis = [cost_x[seed] for seed in common_seeds]
-    # Calculate the point density
-    xy = np.vstack([x_axis, y_axis])
-    z = gaussian_kde(xy)(xy)
-    colors = plt.get_cmap("Blues")((z - z.min()) / (z.max() - z.min()) * 0.75 + 0.50)
-
-    ax.scatter(x_axis, y_axis, c=colors)
-
-    # x axis and y axis should be the same
-    # max_val = max(max(x_axis), max(y_axis))
-    ax.set_xlim(0, max_val)
-    ax.set_ylim(0, max_val)
-    # draw a center line
-    ax.plot([0, max_val], [0, max_val], color='black', linestyle='--', linewidth=0.5, alpha=0.2)
-    # Change tick text size for both x and y axes
-    ax.tick_params(axis='x', labelsize=8)  # Set x-axis tick size
-    ax.tick_params(axis='y', labelsize=8)  # Set y-axis tick size
-
-    # Plot the failed seeds
-    y_fail_seed = set.difference(set(cost_x.keys()), set(cost_y.keys()))
-    y_fail_cost = [cost_x[seed] for seed in y_fail_seed]
-    x_fail_fill_cost = [fail_val for _ in y_fail_seed]
-    if y_fail_cost:
-        print(y_fail_cost)
-        ax.plot([fail_val, fail_val], [min(y_fail_cost) - 30, max(y_fail_cost) + 30], color='black', linestyle='--',
-                linewidth=0.5, alpha=0.2)
-        ax.scatter(x_fail_fill_cost, y_fail_cost, color='red', marker='x')
-
-    x_fail_seed = set.difference(set(cost_y.keys()), set(cost_x.keys()))
-    x_fail_cost = [cost_y[seed] for seed in x_fail_seed]
-    y_fail_fill_cost = [fail_val for _ in x_fail_seed]
-    if x_fail_cost:
-        print(x_fail_cost)
-        ax.plot([min(x_fail_cost) - 30, max(x_fail_cost) + 30], [fail_val, fail_val], color='black', linestyle='--',
-                linewidth=0.5, alpha=0.2)
-        ax.scatter(x_fail_cost, y_fail_fill_cost, color='red', marker='x')
-
-
-def make_scatter_with_box(data_x, data_y, max_val=None):
-    if max_val is None:
-        max_val = 1.1 * max(max(data_x.values()), max(data_y.values()))
-    fail_val = max_val - 20
-    fig8 = plt.figure(constrained_layout=False, figsize=(4, 4))
-    gs1 = fig8.add_gridspec(nrows=8, ncols=8, left=0.1, right=0.9, wspace=0.00, hspace=0)
-    f8_ax_mid = fig8.add_subplot(gs1[:, :])
-    f8_ax_bot = fig8.add_subplot(gs1[-1, :])
-    f8_ax_lft = fig8.add_subplot(gs1[:, 0])
-
-    gjs_scatter_plot(f8_ax_mid, data_x, data_y, max_val, fail_val)
-    # f8_ax_mid.set_xlabel(bot_name)
-    # f8_ax_mid.set_ylabel(lft_name)
-    # f8_ax_mid.set_title(title)
-
-    f8_ax_lft.boxplot(list(data_y.values()), vert=True, showmeans=True)
-    f8_ax_lft.set_ylim([0, max_val])
-    f8_ax_lft.set_axis_off()
-
-    # Bottom
-    f8_ax_bot.boxplot(list(data_x.values()), vert=False, showmeans=True)
-    f8_ax_bot.set_xlim([0, max_val])
-    f8_ax_bot.set_axis_off()
-
-    return f8_ax_mid
+import taskplan
 
 
 if __name__ == "__main__":
@@ -241,40 +21,29 @@ if __name__ == "__main__":
     parser.add_argument('--optimistic_oracle', action='store_true')
     parser.add_argument('--pessimistic_oracle', action='store_true')
     parser.add_argument('--oracle', action='store_true')
-    parser.add_argument('--scatter2', action='store_true')
     args = parser.parse_args()
 
     if args.learned:
-        data = process_learned_data(args)
+        data = taskplan.utilities.result.process_learned_data(args)
         print(data.describe())
     elif args.optimistic_greedy:
-        data = process_optimistic_greedy_data(args)
+        data = taskplan.utilities.result.process_optimistic_greedy_data(args)
         print(data.describe())
     elif args.pessimistic_greedy:
-        data = process_pessimistic_greedy_data(args)
+        data = taskplan.utilities.result.process_pessimistic_greedy_data(args)
         print(data.describe())
     elif args.optimistic_lsp:
-        data = process_optimistic_lsp_data(args)
+        data = taskplan.utilities.result.process_optimistic_lsp_data(args)
         print(data.describe())
     elif args.pessimistic_lsp:
-        data = process_pessimistic_lsp_data(args)
+        data = taskplan.utilities.result.process_pessimistic_lsp_data(args)
         print(data.describe())
     elif args.optimistic_oracle:
-        data = process_optimistic_oracle_data(args)
+        data = taskplan.utilities.result.process_optimistic_oracle_data(args)
         print(data.describe())
     elif args.pessimistic_oracle:
-        data = process_pessimistic_oracle_data(args)
+        data = taskplan.utilities.result.process_pessimistic_oracle_data(args)
         print(data.describe())
     elif args.oracle:
-        data = process_oracle_data(args)
+        data = taskplan.utilities.result.process_oracle_data(args)
         print(data.describe())
-    else:
-        other_data, learned_data = process_data(args)
-        other_result_dict = other_data.set_index('seed').T.to_dict()
-        learned_result_dict = learned_data.set_index('seed').T.to_dict()
-        look_up_str = 'LEARNED_SP' if args.scatter2 else 'NAIVE_LSP'
-        Learned_dict = {k: learned_result_dict[k]['LEARNED_LSP'] for k in learned_result_dict}
-        Other_dict = {k: other_result_dict[k][look_up_str] for k in other_result_dict}
-        plt.clf()
-        make_scatter_with_box(Other_dict, Learned_dict)
-        plt.savefig(args.output_image_file, dpi=600)
