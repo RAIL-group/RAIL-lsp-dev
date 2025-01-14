dbb5ed7333a6c9cf70df8307ebfb0d8f48bcd113
Raihan Islam Arnob <arnob2601@yahoo.com>
2025-01-14 00:27:40 -0500

    removed ortools and its usages in the code for tsp cost calculations

--- modules/taskplan/taskplan/core.py
+++ modules/taskplan/taskplan/core.py
@@ -2,7 +2,6 @@ import math
 import random
 import itertools
 import numpy as np
-from ortools.constraint_solver import pywrapcp, routing_enums_pb2
 
 import gridmap
 import taskplan
@@ -602,40 +601,3 @@ def compute_path_cost(grid, path):
             total_path = np.concatenate((total_path, robot_path), axis=1)
 
     return total_cost, total_path
-
-
-def solve_tsp_for_distance(dist_matrix):
-    # Number of nodes in the problem
-    num_nodes = len(dist_matrix)
-
-    # Create the routing index manager
-    manager = pywrapcp.RoutingIndexManager(num_nodes, 1, 0)
-
-    # Create the routing model
-    routing = pywrapcp.RoutingModel(manager)
-
-    # Create the distance callback function
-    def distance_callback(from_index, to_index):
-        from_node = manager.IndexToNode(from_index)
-        to_node = manager.IndexToNode(to_index)
-        return dist_matrix[from_node][to_node]
-
-    # Register the distance callback
-    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
-
-    # Define the cost of each arc
-    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
-
-    # Set the search parameters
-    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    search_parameters.first_solution_strategy = (
-        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
-
-    # Solve the problem
-    solution = routing.SolveWithParameters(search_parameters)
-
-    # If a solution is found, return the total distance
-    if solution:
-        return solution.ObjectiveValue()
-    else:
-        return 10000
