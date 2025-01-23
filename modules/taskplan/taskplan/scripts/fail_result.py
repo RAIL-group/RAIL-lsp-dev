80a9b2a7912697ed7c907c74bb216671e78cb86a
Raihan Islam Arnob <arnob2601@yahoo.com>
2025-01-23 15:46:53 -0500

    updated script to process successrates

--- modules/taskplan/taskplan/scripts/fail_result.py
+++ modules/taskplan/taskplan/scripts/fail_result.py
@@ -1,4 +1,5 @@
 import os
+import re
 import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,6 +7,24 @@ import matplotlib.pyplot as plt
 import taskplan
 
 pd.set_option('display.max_columns', None)
+
+
+def get_target_seeds(file_name):
+    values = []
+    with open(file_name, 'r') as file:
+        for line in file:
+            match = re.search(r's: (\d{4})', line)
+            if match:
+                values.append(int(match.group(1)))
+    return sorted(values)[:100]
+
+
+def filter(d_dict, target_seeds):
+    return {k: v for k, v in d_dict.items() if k in target_seeds}
+
+
+def get_success_rate(val):
+    return (100 - val) / 100
 
 
 if __name__ == "__main__":
@@ -25,17 +44,28 @@ if __name__ == "__main__":
                         required=False, default=None)
     args = parser.parse_args()
 
+    target_maps = get_target_seeds(args.df_oracle)
+
     args.data_file = args.df_learned
     data = taskplan.utilities.result.process_learned_data(args)
     learned_dict = data.set_index('seed')['LEARNED_LSP'].to_dict()
-    dicts = []
-    look_up_strs = []
+    learned_dict = filter(learned_dict, target_maps)
+
+    look_up_str = 'ORACLE'
+    args.data_file = args.df_oracle
+    data = taskplan.utilities.result.process_oracle_data(args)
+    other_dict = data.set_index('seed')[look_up_str].to_dict()
+    other_dict = filter(other_dict, target_maps)
+
+    dicts = [other_dict]
+    look_up_strs = [look_up_str]
 
     if os.path.exists(args.df_opt_greedy):
         look_up_str = 'OPTIMISTIC_GREEDY'
         args.data_file = args.df_opt_greedy
         data = taskplan.utilities.result.process_optimistic_greedy_data(args)
         other_dict = data.set_index('seed')[look_up_str].to_dict()
+        other_dict = filter(other_dict, target_maps)
         dicts.append(other_dict)
         look_up_strs.append(look_up_str)
         plt.clf()
@@ -48,6 +78,7 @@ if __name__ == "__main__":
         args.data_file = args.df_pes_greedy
         data = taskplan.utilities.result.process_pessimistic_greedy_data(args)
         other_dict = data.set_index('seed')[look_up_str].to_dict()
+        other_dict = filter(other_dict, target_maps)
         dicts.append(other_dict)
         look_up_strs.append(look_up_str)
         plt.clf()
@@ -60,6 +91,7 @@ if __name__ == "__main__":
         args.data_file = args.df_opt_lsp
         data = taskplan.utilities.result.process_optimistic_lsp_data(args)
         other_dict = data.set_index('seed')[look_up_str].to_dict()
+        other_dict = filter(other_dict, target_maps)
         dicts.append(other_dict)
         look_up_strs.append(look_up_str)
         plt.clf()
@@ -72,6 +104,7 @@ if __name__ == "__main__":
         args.data_file = args.df_pes_lsp
         data = taskplan.utilities.result.process_pessimistic_lsp_data(args)
         other_dict = data.set_index('seed')[look_up_str].to_dict()
+        other_dict = filter(other_dict, target_maps)
         dicts.append(other_dict)
         look_up_strs.append(look_up_str)
         plt.clf()
@@ -90,6 +123,17 @@ if __name__ == "__main__":
     merged_df = dfs[0]
     for df in dfs[1:]:
         merged_df = pd.merge(merged_df, df, on='seed', how='outer')
+
+    fc_opt_grdy = merged_df['OPTIMISTIC_GREEDY'].isna().sum()
+    fc_pes_grdy = merged_df['PESSIMISTIC_GREEDY'].isna().sum()
+    fc_opt_lsp = merged_df['OPTIMISTIC_LSP'].isna().sum()
+    fc_pes_lsp = merged_df['PESSIMISTIC_LSP'].isna().sum()
+    fc_learned = merged_df['LEARNED_LSP'].isna().sum()
+    print(f"Succes rate for OPTIMISTIC_GREEDY: {get_success_rate(fc_opt_grdy)}")
+    print(f"Succes rate for PESSIMISTIC_GREEDY: {get_success_rate(fc_pes_grdy)}")
+    print(f"Succes rate for OPTIMISTIC_LSP: {get_success_rate(fc_opt_lsp)}")
+    print(f"Succes rate for PESSIMISTIC_LSP: {get_success_rate(fc_pes_lsp)}")
+    print(f"Succes rate for LEARNED_LSP: {get_success_rate(fc_learned)}")
 
     # Fill missing values with 5000
     merged_df.fillna(8000, inplace=True)
