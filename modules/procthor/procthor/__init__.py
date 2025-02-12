eb3b843bd26248ca58625cf4ced74eba553732b9
Abhishek Paudel <10024463+abpaudel@users.noreply.github.com>
2025-02-12 10:27:33 -0500

    add utils

--- modules/procthor/procthor/__init__.py
+++ modules/procthor/procthor/__init__.py
@@ -1,2 +1,3 @@
-from .procthor import ThorInterface
-from . import plotting
+from .procthor import ThorInterface  # noqa: F401
+from . import plotting  # noqa: F401
+from . import utils  # noqa: F401
