41933b4922975f9f2b9fdae15200dffb52182ec4
Abhishek Paudel <10024463+abpaudel@users.noreply.github.com>
2025-02-18 16:15:29 -0500

    move scene graph

--- modules/procthor/procthor/__init__.py
+++ modules/procthor/procthor/__init__.py
@@ -2,3 +2,4 @@ from .procthor import ThorInterface  # noqa: F401
 from . import simulators  # noqa: F401
 from . import plotting  # noqa: F401
 from . import utils  # noqa: F401
+from . import scenegraph  # noqa: F401
