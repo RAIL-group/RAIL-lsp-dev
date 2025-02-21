28a14e94330c0476a035e8481e2163e22c7e95e2
Abhishek Paudel <10024463+abpaudel@users.noreply.github.com>
2025-02-21 16:17:57 -0500

    rm observed grid and use chosen_container_idx

--- modules/procthor/procthor/simulators.py
+++ modules/procthor/procthor/simulators.py
@@ -22,45 +22,44 @@ class SceneGraphSimulator:
 
         return self.thor_interface.get_top_down_image(orthographic=orthographic)
 
-    def initialize_graph_map_and_subgoals(self):
+    def initialize_graph_and_containers(self):
         random.seed(self.args.current_seed)
         # Select half of the containers as subgoals or at least two
         cnt_count = len(self.known_graph.container_indices)
         lb_sample = min(cnt_count, 2)
         num_of_val_to_choose = max(lb_sample, random.sample(list(range(
             cnt_count // 2, cnt_count)), 1)[0])
-        subgoals = random.sample(self.known_graph.container_indices, num_of_val_to_choose)
+        unexplored_containers = random.sample(self.known_graph.container_indices, num_of_val_to_choose)
         for cnt_idx in self.target_obj_info['container_idx']:
-            if cnt_idx not in subgoals:
-                subgoals.append(cnt_idx)
-        subgoals = sorted(subgoals)
+            if cnt_idx not in unexplored_containers:
+                unexplored_containers.append(cnt_idx)
+        unexplored_containers = sorted(unexplored_containers)
 
-        observed_grid = self.known_grid.copy()  # For now, observed grid is the same as known grid
         observed_graph = self.known_graph.get_object_free_graph()
 
         # Reveal container nodes not chosen as subgoals
         cnt_to_reveal_idx = [xx
                              for xx in self.known_graph.container_indices
-                             if xx not in subgoals]
+                             if xx not in unexplored_containers]
         for node_idx in cnt_to_reveal_idx:
             connected_obj_idx = self.known_graph.get_adjacent_nodes_idx(node_idx, filter_by_type=3)
             for obj_idx in connected_obj_idx:
                 o_idx = observed_graph.add_node(self.known_graph.nodes[obj_idx].copy())
                 observed_graph.add_edge(node_idx, o_idx)
 
-        return observed_graph, observed_grid, subgoals
+        return observed_graph, unexplored_containers
 
-    def update_graph_map_and_subgoals(self, observed_graph, observed_grid, subgoals, chosen_subgoal=None):
-        if chosen_subgoal is None:
-            return observed_graph, observed_grid, subgoals
+    def update_graph_and_containers(self, observed_graph, containers, chosen_container_idx=None):
+        if chosen_container_idx is None:
+            return observed_graph, containers
 
-        subgoals = [s for s in subgoals if s != chosen_subgoal.id]
+        unexplored_containers = [s for s in containers if s != chosen_container_idx]
         graph = observed_graph.copy()
 
         # Add objects from chosen container to the graph
-        connected_obj_idx = self.known_graph.get_adjacent_nodes_idx(chosen_subgoal.id, filter_by_type=3)
+        connected_obj_idx = self.known_graph.get_adjacent_nodes_idx(chosen_container_idx, filter_by_type=3)
         for obj_idx in connected_obj_idx:
             o_idx = graph.add_node(self.known_graph.nodes[obj_idx].copy())
-            graph.add_edge(chosen_subgoal.id, o_idx)
+            graph.add_edge(chosen_container_idx, o_idx)
 
-        return graph, observed_grid, subgoals
+        return graph, unexplored_containers
