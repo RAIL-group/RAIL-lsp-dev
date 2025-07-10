ea8e2bc57bf1f3999709fce94373480ac49f860b
Eddielin9876 <75624242+onetwothreefourfivesixe@users.noreply.github.com>
2025-07-10 13:24:57 -0400

    Co-authored-by: VennelaMandava <VennelaMandava@users.noreply.github.com> Co-authored-by: rohanpeddireddy <rohanpeddireddy@users.noreply.github.com>

---
+++ modules/object_search/object_search/planners/assipUtils.py
@@ -0,0 +1,27 @@
+
+class State():
+    """Position of robot and dictionary of containers mapped to what’s in them) """
+    def __init__(self, robot_pose, containers):
+        self.robot_pose:tuple[int] = robot_pose
+        self.containers:list[Container] = containers
+
+    def getListOfContainerLocations(self):
+        """Returns a list of all container locations."""
+        return {container.name : container.location for container in self.containers}
+    
+    def getListOfKnownObjects(self):
+        """Returns a list of all objects in all containers."""
+        return [obj for container in self.containers for obj in container.objects]
+    
+class Container():
+    """Container with a list of objects in it."""
+    def __init__(self, name, location, objects):
+        self.name = name
+        self.location = location
+        self.objects = objects
+    
+    def __repr__(self):
+        return f"Container({self.name}, {self.objects})"
+    
+    def __str__(self):
+        return f"Container: {self.name} with objects: {self.objects}"
