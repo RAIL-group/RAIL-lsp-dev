e696e6f3ffa49b16784c5713a41316b8fdf6e9cf
Raihan Islam Arnob <arnob2601@yahoo.com>
2026-02-26 21:22:43 -0500

    updated training script to have 500 epochs

--- modules/sctp/sctp/scripts/iapgnn_training.py
+++ modules/sctp/sctp/scripts/iapgnn_training.py
@@ -13,7 +13,7 @@ from torch_geometric.data import Data
 class GzipGNNDataset(Dataset):
     def __init__(self, folder_path):
         self.file_paths = glob.glob(os.path.join(folder_path, '*.pgz'))
-        
+
     def __len__(self):
         return len(self.file_paths)
 
@@ -30,37 +30,37 @@ class GzipGNNDataset(Dataset):
         if hasattr(raw_data, 'graph_metadata'):
             data.metadata = raw_data.graph_metadata
         return data
-        
+
 
 
 def train_epoch(model, loader, optimizer, criterion, device):
     model.train()
     total_loss = 0
-    
+
     for batch in loader:
         batch = batch.to(device)
         optimizer.zero_grad()
-        
+
         # 2. Forward Pass
         # Pass the batch data and the specific node attributes
         output = model(
-            x= batch.x, 
+            x= batch.x,
             edge_index=batch.edge_index,
             edge_attr=batch.edge_attr,
         )
-        
+
         # 3. Compute Loss
         # Ensure output and target have the same shape [Batch, 1]
         # loss = criterion(output.view(-1), batch.y.view(-1))
         loss = criterion(output, batch.y)  # MSE Loss between predicted and true edge values
-        
+
         # 4. Backward Pass
         loss.backward()
         optimizer.step()
-        
+
         # total_loss += loss.item() * batch.num_graphs
         total_loss += loss.item()
-        
+
     return total_loss / len(loader)
 
 
@@ -74,18 +74,18 @@ if __name__ == "__main__":
     train_loader = DataLoader(dataset, batch_size=8, shuffle=True)
 
     # 1. Setup Device & Model
-    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    device = torch.device('cpu')  # Force CPU for debugging
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # device = torch.device('cpu')  # Force CPU for debugging
     NODE_IN = 2
     EDGE_IN = 2
     HIDDEN = 32
     model = BipartiteEdgeRegressor(node_in_dim=NODE_IN, edge_in_dim=EDGE_IN, hidden_dim=HIDDEN).to(device)
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=0.0001)
     criterion = nn.MSELoss()
-    
+
     # 5. Execute Training
-    num_epochs = 50
+    num_epochs = 500
     for epoch in range(1, num_epochs + 1):
         loss = train_epoch(model, train_loader, optimizer, criterion, device)
-        if epoch % 10 == 0:
-            print(f'Epoch: {epoch:03d}, MSE Loss: {loss:.4f}')
+        # if epoch % 10 == 0:
+        print(f'Epoch: {epoch:03d}, MSE Loss: {loss:.4f}')
