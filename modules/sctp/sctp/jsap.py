9cad51a3430f9d6bf1e8e8bcebb15a441a2684d9
buivn <hbui20@gmu.edu>
2026-02-27 12:00:15 -0500

    add some experiments

--- modules/sctp/sctp/jsap.py
+++ modules/sctp/sctp/jsap.py
@@ -40,7 +40,7 @@ class JSAPState(object):
             assert ugvs != []
             self.graph = graph
             self.max_uanum = max_uanum
-            assert self.max_uanum == 3
+            assert self.max_uanum == 4
             self.goalIDs = goalIDs
             self.history = core.History()
             self.vertices_map = {v.id: v for v in self.graph.vertices + self.graph.pois}
@@ -296,7 +296,7 @@ class JSAPState(object):
         if new_state.use_AVP or new_state.use_DAP:
             assert new_state.use_AVP != new_state.use_DAP
         new_state.max_uanum = self.max_uanum
-        assert new_state.max_uanum == 3
+        assert new_state.max_uanum == 4
         new_state.action_cost = 0.0
         new_state.revisit_pen = self.revisit_pen
         new_state.assigned_pois = self.assigned_pois.copy() # [poi for poi in self.assigned_pois]
